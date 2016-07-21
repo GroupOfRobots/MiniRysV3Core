9de55ae79c3bb66fec3a652efdbe3989b24a0361
kamfor <kfforex@gmail.com>
2016-07-21 16:42:12 +0200

    message receiver

--- DebugSocket/server.py
+++ DebugSocket/server.py
@@ -1,30 +1,40 @@
+import datetime
 import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import socket
-'''
-This is a simple Websocket Echo server that uses the Tornado websocket handler.
-Please run `pip install tornado` with python of version 2.7.9 or greater to install tornado.
-This program will echo back the reverse of whatever it recieves.
-Messages are output to the terminal for debuggin purposes. 
-''' 
- 
+
+
+
+def send_to_all_clients(msg):
+    for client in clients:
+        client.write_message(message)
+
 class WSHandler(tornado.websocket.WebSocketHandler):
+
+    clients = []
     def open(self):
         print 'new connection'
+	self.write_message("Hello World")
+        WSHandler.clients.append(self)
       
     def on_message(self, message):
         print 'message received:  %s' % message
-        # Reverse Message and send it back
-        print 'sending back message: %s' % message[::-1]
-        self.write_message(message[::-1])
+	#self.write_message('ECHO: ' + message)
  
     def on_close(self):
         print 'connection closed'
+	WSHandler.clients.remove(self)
  
     def check_origin(self, origin):
         return True
+
+    @classmethod
+    def write_to_clients(cls):
+        print "Writing to clients"
+        for client in cls.clients:
+            client.write_message("Hello")
  
 application = tornado.web.Application([
     (r'/ws', WSHandler),
@@ -36,6 +46,7 @@ if __name__ == "__main__":
     http_server.listen(8888)
     myIP = socket.gethostbyname(socket.gethostname())
     print '*** Websocket Server Started at %s***' % myIP
+    #tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=15), WSHandler.write_to_clients)
     tornado.ioloop.IOLoop.instance().start()
  
 
