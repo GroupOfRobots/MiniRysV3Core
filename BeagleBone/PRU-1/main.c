6f4f5b280308416b647aa8ca29825eefe983a692
kamfor <kfforex@gmail.com>
2016-11-12 00:57:48 +0100

    rproc work:

--- BeagleBone/PRU-1/main.c
+++ BeagleBone/PRU-1/main.c
@@ -1,80 +1,140 @@
-/***************************************************************************************
- * MAIN.C
+/*
+ * Copyright (C) 2016 Texas Instruments Incorporated - http://www.ti.com/
  *
- * Description: main source file for PRU development
  *
- * Rafael de Souza
- * (C) 2015 Texas Instruments, Inc
- * 
- * Built with Code Composer Studio v6
- **************************************************************************************/
-
-//create shared memory pointer and read speed and dir of mototr,
-//distance sensor on PRU0
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the
+ *	  distribution.
+ *
+ *	* Neither the name of Texas Instruments Incorporated nor the names of
+ *	  its contributors may be used to endorse or promote products derived
+ *	  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pru_cfg.h>
 #include <pru_intc.h>
-#include <pru_ctrl.h>
-#include <pru_iep.h>
+#include <rsc_types.h>
+#include <pru_rpmsg.h>
+#include "resource_table_1.h"
 
-struct motor_data{
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
 
-    int leftSpeed;
-    int rightSpeed;
-    int leftDir;
-    int rightDir;
-    int leftSteps;
-    int rightSteps;
-    int resetSteps;
-};
 
-volatile register unsigned int __R31, __R30;
-volatile unsigned int stepsL;
-volatile unsigned int stepsR;
+/* Host-1 Interrupt sets bit 31 in register R31 */
+#define HOST_INT			((uint32_t) 1 << 31)	
 
-int main(void) {
-    unsigned int loops, delay;
+/* The PRU-ICSS system events used for RPMsg are defined in the Linux device tree
+ * PRU0 uses system event 16 (To ARM) and 17 (From ARM)
+ * PRU1 uses system event 18 (To ARM) and 19 (From ARM)
+ */
+#define TO_ARM_HOST			18	
+#define FROM_ARM_HOST			19
 
-    /* allow OCP master port access by the PRU so the PRU can read external memories */
-    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+#define LSTEP				7 //P8_40
+#define LDIR				6 //P8_39
+#define RSTEP				3 //P8_44
+#define RDIR				2 //P8_43
 
-    void *p = (void*)0x00002000;
+#define REN					0 //P8_45
+#define LEN					1 //P8_46
 
-    struct motor_data* share_buff = (struct motor_data*)p;
+/*
+ * Using the name 'rpmsg-client-sample' will probe the RPMsg sample driver
+ * found at linux-x.y.z/samples/rpmsg/rpmsg_client_sample.c
+ *
+ * Using the name 'rpmsg-pru' will probe the rpmsg_pru driver found
+ * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
+ */
+//#define CHAN_NAME			"rpmsg-client-sample"
+#define CHAN_NAME			"rpmsg-pru"
 
-    //struct motor_data share_buff;
+#define CHAN_DESC			"Channel 31"
+#define CHAN_PORT			31
 
-    while(1){
-		for (loops = 0; loops < 10000; loops++) {
+/*
+ * Used to make sure the Linux drivers are ready for RPMsg communication
+ * Found at linux-x.y.z/include/uapi/linux/virtio_config.h
+ */
+#define VIRTIO_CONFIG_S_DRIVER_OK	4
 
-			if(loops%share_buff->leftSpeed){
-				__R30 = __R30 | (1 << 0); // Toogle step
-				stepsL++;
+uint8_t payload[RPMSG_BUF_SIZE];
+
+/*
+ * main.c
+ */
+void main(void)
+{
+	struct pru_rpmsg_transport transport;
+	uint16_t src, dst, len;
+	volatile uint8_t *status;
+	int i;
+	int j=0;
+	int k=0;
+	int ww =4000;
+	int speed =12000;
+
+	/* Allow OCP master port access by the PRU so the PRU can read external memories */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	/* Clear the status of the PRU-ICSS system event that the ARM will use to 'kick' us */
+	CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+
+	/* Make sure the Linux drivers are ready for RPMsg communication */
+	status = &resourceTable.rpmsg_vdev.status;
+	while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+
+	/* Initialize the RPMsg transport structure */
+	pru_rpmsg_init(&transport, &resourceTable.rpmsg_vring0, &resourceTable.rpmsg_vring1, TO_ARM_HOST, FROM_ARM_HOST);
+
+	/* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+	while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+	while (1) {
+		++ww;
+		/* Check bit 30 of register R31 to see if the ARM has kicked us */
+		if (__R31 & HOST_INT) {
+			/* Clear the event status */
+			CT_INTC.SICR_bit.STS_CLR_IDX = FROM_ARM_HOST;
+			/* Receive all available messages, multiple messages can be sent per kick */
+			while (pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS) {
+				/* Echo the message back to the same address from which we just received */
+				pru_rpmsg_send(&transport, dst, src, payload, len);
+
 			}
-			if(loops%share_buff->rightSpeed){
-				__R30 = __R30 | (1 << 0); // Toogle step
-				stepsL++;
-			}
-
-			for (delay = 0; delay < 200; delay++);
-			__R30 = __R30 & ~(1 << 0); // Toggle off
-			__R30 = __R30 & ~(1 << 1); // Toggle off
-
-			//Setting direction
-			if(share_buff->leftDir) __R30 = __R30 | (1 << 0);
-			else __R30 = __R30 & ~(1 << 0);
-			if(share_buff->rightDir) __R30 = __R30 | (1 << 0);
-			else __R30 = __R30 & ~(1 << 0);
-
-			if(share_buff->resetSteps)stepsL = stepsR = 0;
-			share_buff->rightSteps = stepsR;
-			share_buff->leftSteps = stepsL;
+			speed = atoi((const char*)payload);
 		}
-    }
-
-    // Send interrupt to host and halt
-    __R31 = 32 | 3;
-    __halt();
-    return 0;
+		for(i=0; i<50000; ++j, ++i, ++k){
+			if(j>=ww){
+				__R30 = __R30 ^ (1 << LEN);
+				j=0;
+			}
+			if(k>=speed){
+				__R30 = __R30 ^ (1 << REN);
+				k=0;
+			}
+		}
+	}
 }
