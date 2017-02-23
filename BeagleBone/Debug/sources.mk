13ddada849a6976402a5e57b62a1ec3c2b6d67e9
kamfor <kamfor>
2017-02-23 01:29:00 +0100

    chainname

--- BeagleBone/Debug/sources.mk
+++ BeagleBone/Debug/sources.mk
@@ -23,10 +23,10 @@ CPP_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Battery \
-Controller \
-IMU \
-Motors \
-Sonar \
 . \
+Sonar \
+Motors \
+IMU \
+Controller \
+Battery \
 
