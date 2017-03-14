1434c2fa62102e5b3ca1b4a6e0ab8153c10e98f1
kamfor <kfforex@gmail.com>
2017-03-14 16:10:51 +0100

    more examples

--- BeagleBone/TestApplication.cpp
+++ BeagleBone/TestApplication.cpp
@@ -1,37 +1,8 @@
 /*
- * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com/
- *
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *	* Redistributions of source code must retain the above copyright
- *	  notice, this list of conditions and the following disclaimer.
- *
- *	* Redistributions in binary form must reproduce the above copyright
- *	  notice, this list of conditions and the following disclaimer in the
- *	  documentation and/or other materials provided with the
- *	  distribution.
- *
- *	* Neither the name of Texas Instruments Incorporated nor the names of
- *	  its contributors may be used to endorse or promote products derived
- *	  from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Wake up example
  */
 
-/*
+
 #include <stdlib.h>
 #include <inttypes.h>
 #include <stdio.h>
@@ -44,30 +15,36 @@
 #include <stdio.h>
 #include <string>
 #include <memory>
+#include <iostream>
 
 
 #include "Motors/motors.h" //motors class
 #include "Battery/battery.h" //Battery measurement class
 #include "IMU/imu.h" // angle measurement class
 #include "Controller/controller.h" //angle & speed control
-#include "Sonar/sonar.h" //distance sensors class
-#include "Socket/socket.h" //websocket communication
 
 
 Motors silniki;
 Battery lipol;
 Imu imu;
-Sonar hcr;
 Controller pid;
 float angle;
+float actualangle;
 int nowyspeed=0;
 float finalleftspeed;
 float finalrightspeed;
 int steering;
 int throttle;
 
-void balancing(){
+ enum Position{
+	 layfront = 0,
+	 layback = 1,
+	 standing = 2,
+	 check = 3
+ };
 
+
+void balancing(){ //thread balancing function
 
 	while(1){
 		angle += imu.getRoll();
@@ -78,103 +55,74 @@ void balancing(){
 		usleep(50);
 		angle += imu.getRoll();
 		usleep(50);
-		angle = angle/4*180/3.1415;
+		angle = actualangle =angle/4*180/3.1415;
 		pid.calculate_speed(angle,silniki.getLeftSpeed(),silniki.getRightSpeed(),steering,throttle,finalleftspeed,finalrightspeed);
 		silniki.setSpeed(finalleftspeed,finalrightspeed,0.0,4);
 		angle = 0.0;
 	}
 }
 
-char getch(){
-    char buf=0;
-    struct termios old={0};
-    fflush(stdout);
-    if(tcgetattr(0, &old)<0)
-        perror("tcsetattr()");
-    old.c_lflag&=~ICANON;
-    old.c_lflag&=~ECHO;
-    old.c_cc[VMIN]=1;
-    old.c_cc[VTIME]=0;
-    if(tcsetattr(0, TCSANOW, &old)<0)
-        perror("tcsetattr ICANON");
-    if(read(0,&buf,1)<0)
-        perror("read()");
-    old.c_lflag|=ICANON;
-    old.c_lflag|=ECHO;
-    if(tcsetattr(0, TCSADRAIN, &old)<0)
-        perror ("tcsetattr ~ICANON");
-    printf("%c\n",buf);
-    return buf;
- }
-
 int main(void)
 {
 
 	silniki.disable();
 	imu.setup(); // initialize IMU
-	usleep(100000);
+	usleep(1000);
 	silniki.enable();
-
-
 	//if(!lipol.isGood())printf("niski poziom napiecia baterii");
 	imu.resetFIFO();
+	pid.timerStart(); //start pid timer
+
+	//std::thread balance(balancing);
+	//balance.detach();
+
+	Position actual;
 
 
+	while(1){
 
-
-	char c;
-	int exit=1;
-	pid.timerStart();
-
-
-	std::thread balance(balancing);
-
-	while(exit){
-
-		//printf("ADC5: %d\n",lipol.getRaw());
-
-
-		switch (c){
-		case 'w':
-			if(throttle<0)throttle = 0;
-			throttle+=5;
+		switch (actual){
+		case check:
+			imu.resetFIFO();
+			actualangle = imu.getRoll();
+			actualangle =actualangle*180/3.1415;
+			std::cout<<"my angle: "<<actualangle<<std::endl;
+			if(actualangle>40.0)actual = Position::layfront;
+			else if (actualangle<-40.0)actual = Position::layfront;
+			else actual = Position::standing;
 		break;
-		case 'a':
-			steering +=1;
+		case standing:
+			//balance.join();
+			std::cout<<"I'm standing"<<std::endl;
+			usleep(2000000);
+			actual = Position::check;
 		break;
-		case 's':
-			if(throttle>0)throttle =0;
-			throttle-=1;
+		case layfront:
+			std::cout<<"I'm trying to stand front"<<std::endl;
+			//balance.detach();
+			usleep(5000000);
+			silniki.setSpeed(-800.0,-800.0,0.0,1);
+			usleep(500000);
+			silniki.setSpeed(800.0,800.0,0.0,1);
+			usleep(100000);
+			//balance.join();
+			actual = Position::check;
 		break;
-		case 'd':
-			steering-=1;
-		break;
-		case 'q':
-			silniki.disable();
-			exit =0;
-			usleep(10000);
-		break;
-		case 'r':
-			steering=0;
-			throttle=0;
-		break;
-		default:
-
-			usleep(1000);
-			//silniki.setSpeed(100,100,3);
-			//usleep(100000);
-
-			//printf("Message received from PRU:%d\n", hcr.getDistance(distance_sensors::front));
-			//printf("ADC5: %d\n",lipol.getRaw());
+		case layback:
+			std::cout<<"I'm trying to stand back"<<std::endl;
+			//balance.detach();
+			usleep(5000000);
+			silniki.setSpeed(800.0,800.0,0.0,1);
+			usleep(500000);
+			silniki.setSpeed(-800.0,-800.0,0.0,1);
+			usleep(100000);
+			//balance.join();
+			actual = Position::check;
 		break;
 		}
-
-		silniki.setSpeed(throttle,throttle,0.0,1);
-
-		c = getch();
 	}
 
-	balance.detach();
+	//balance.detach();
 	return 0;
 }
-*/
+
